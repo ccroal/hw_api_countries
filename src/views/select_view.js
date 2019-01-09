e367c0347905b5eeec6ae007bfc4547f6a6b6cc9
Christopher Croal <ccroal@ntlworld.com>
2019-01-09 16:56:42 +0000

    added country names to dropdown in selectview

--- src/views/select_view.js
+++ src/views/select_view.js
@@ -6,8 +6,17 @@ const SelectView = function(element){
 
 SelectView.prototype.bindEvents = function(){
   PubSub.subscribe('Countries:all-data', (event) => {
-    this.populate(event.details);
-  })
+    this.populate(event.detail);
+    console.log('Event details', event.detail)
+  });
 }
 
+SelectView.prototype.populate = function(countries){
+  countries.forEach((country, index) => {
+    const option = document.createElement('option');
+    option.value = index
+    option.textContent = country.name
+    this.element.appendChild(option)
+  })
+}
 module.exports = SelectView;
